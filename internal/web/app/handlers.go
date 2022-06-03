997bfd4bd393ce4c1a4024e718924e42933c6b63
tmwilder <tmwilder@gmail.com>
2022-06-02 22:26:29 -0700

    Start interpreting result

--- internal/web/app/handlers.go
+++ internal/web/app/handlers.go
@@ -1,12 +1,16 @@
 package app
 
 import (
+	"fmt"
 	. "github.com/tmwilder/wh3-draftbot/internal/algo"
 	. "github.com/tmwilder/wh3-draftbot/internal/common"
 	"html/template"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"strconv"
+	"strings"
 )
 
 type pageData struct {
@@ -25,6 +29,9 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
+	parseInputs(r)
+
 	tournamentInfo := TournamentInfo{RoundCount: 3, MatchupOdds: MatchupsV1d2}
 	err = t.Execute(w,
 		pageData{
@@ -40,9 +47,6 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 					Ban:        OK,
 					CounterBan: OK,
 					Matchup:    Matchup{P1: KH, P2: SL}}},
-			RenderNewP2:      true,
-			RenderNewP3:      true,
-			RenderExistingP3: false,
 		},
 	)
 	if err != nil {
@@ -72,6 +76,36 @@ func recommendHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+}
+
+func parseInputs(r *http.Request) (TournamentInfo, GameState) {
+	queryParams, err := url.ParseQuery(r.URL.RawQuery)
+	if err != nil {
+		panic("Cannot parse input: " + err.Error())
+	}
+	// Extract matchup odds
+	// We'll do it the gross way so we can remember life without tools ;)
+	matchupOdds := map[Matchup]float64{}
+	for k, v := range queryParams {
+		if len(k) == 5 && k[2] == '-' {
+			split := strings.Split(k, "-")
+			odds, err := strconv.ParseFloat(v[0], 64)
+			if err != nil {
+				panic("Cannot parse input: " + err.Error())
+			}
+			matchupOdds[Matchup{P1: Faction(split[0]), P2: Faction(split[1])}] = odds
+		}
+	}
+
+	roundsStr := queryParams.Get("rounds")
+	roundCount, err := strconv.ParseInt(roundsStr, 0, 64)
+	if err != nil {
+		fmt.Println("Cannot parse input: " + err.Error())
+		roundCount = 3
+	}
+	tournamentInfo := TournamentInfo{RoundCount: int(roundCount), MatchupOdds: matchupOdds}
+	// Add gameInfo parse - maybe go to web framework/Gin here ;)
+	return tournamentInfo, GameState{}
 }
 
 func loadTemplate(templateName string) (*template.Template, error) {
